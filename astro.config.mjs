1cc8a69aae295441d8c40e6165ac49642e06e69d
Matthew Antone <matt.antone@gmail.com>
2023-11-30 14:22:16 -0800

    updated tina version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,38 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
+import react from "@astrojs/react";
+
+
+const tina = ({
+  directiveName = 'tina'
+} = {}) => ({
+  name: 'tina-cms',
+  hooks: {
+    'astro:config:setup': ({
+      addClientDirective,
+    }) => {
+      addClientDirective({
+        name: directiveName,
+        entrypoint: './client-directives/tina.mjs'
+      });
+    }
+  },
+  vite: {
+    ssr: {
+      noExternal: ['path-to-regexp'],
+    },
+  },
+});
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://asro-tina.com',
+  output: "hybrid",
+  adapter: netlify(),
+  integrations: [ mdx(), react(), sitemap(), tailwind({
+    applyBaseStyles: true,
+  }), tina() ]
+});
---
+++ client-directives/tina.mjs
@@ -0,0 +1,16 @@
+/**
+ * @type {import('astro').ClientDirective}
+ */
+
+
+export default (load) => {
+  try {
+    const isEditor =
+      window.frameElement && window.frameElement.id === "tina-iframe";
+    if (isEditor) {
+      load().then((hydrate) => hydrate());
+    }
+  } catch (e) {
+    console.error(e);
+  }
+};
