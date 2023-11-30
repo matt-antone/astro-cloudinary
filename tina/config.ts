25db5525891c51daf1f824064867ee81e7eaf236
Matthew Antone <matt.antone@gmail.com>
2023-11-30 14:00:19 -0800

    test files

---
+++ tina/config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+  
+  const media = {
+    loadCustomStore: async () => {
+      const pack = await import("next-tinacms-cloudinary");
+      return pack.TinaCloudCloudinaryMediaStore;
+    },
+  }
+
+export default defineConfig({
+  branch,
+
+  // Get this from tina.io
+  clientId: process.env.PUBLIC_TINA_CLIENT_ID,
+  // Get this from tina.io
+  token: process.env.PUBLIC_TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media,
+  // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: "image",
+            name: 'image',
+            label: 'Image',
+          }
+        ],
+      },
+    ],
+  },
+});
